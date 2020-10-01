c120dbb99123c1b1a6bafad5d970df6979e5a3b6
Marcos Willian <markusuuuu@gmail.com>
2020-10-01 15:59:49 -0300

    modified type of column

--- server/src/database/migrations/1601559540125-CreateUsers.ts
+++ server/src/database/migrations/1601559540125-CreateUsers.ts
@@ -10,7 +10,7 @@ export default class CreateUsers1601559540125 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
