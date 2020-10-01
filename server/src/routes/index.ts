59379db4a6023c61f867d2d8a424afa8fc53a359
Marcos Willian <markusuuuu@gmail.com>
2020-10-01 15:59:13 -0300

    😁 created route for user registration 😁

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import studentsRouter from './students.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/students', studentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
