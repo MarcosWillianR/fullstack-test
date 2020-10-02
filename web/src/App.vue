97e4574ca365820cd70e845ea19c68e732c4615b
Marcos Willian <markusuuuu@gmail.com>
2020-10-02 11:51:06 -0300

    initial commit vue web application

---
+++ web/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
+
+html, body {
+  height: 100%;
+  max-height: 100vh;
+}
+
+body {
+  background: #fff;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #003249;
+}
+
+body, input, button {
+  font-family: 'Roboto', sans-serif;
+  font-size: 16px;
+}
+
+h1, h2, h3, h4, h5, h6, strong {
+  font-weight: 700;
+}
+
+button {
+  cursor: pointer;
+}
+
+img {
+  max-width: 100%;
+  max-height: 100vh;
+  margin-bottom: -4px;
+}
+</style>
---
+++ web/src/views/SignIn.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="signIn">
+    <div id="form-container">
+      <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+    </div>
+
+    <img src="../assets/SignInBackground.png" />
+  </section>
+</template>
+
+<style scoped>
+  .signIn {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default defineComponent({
+  name: 'Home',
+  // components: {
+  //   HelloWorld,
+  // },
+});
+</script>
